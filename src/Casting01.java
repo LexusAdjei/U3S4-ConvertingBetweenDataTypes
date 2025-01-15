8d22bd127cd5f35322cf575c59b16a76545fb238
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2025-01-15 17:00:22 -0500

    Changes changes
    changes

--- src/Casting01.java
+++ src/Casting01.java
@@ -1,20 +1,27 @@
 public class Casting01 {
     public static void main(String[] args) {
+
         //Declare and initialize a byte with a value of 128
+        //byte myAByte = 128;
         //Observe NetBeans' complaint
 
         
         //Declare and initialize a short with a value of 128
+        short myBShort = 128;
+
         //Create a print statement that casts this short to a byte
+        byte casted = (byte) myBShort;
+        System.out.println("Casted btye from short" + casted);
 
-        
-        
+
         //Declare and initialize a byte with a value of 127
-        //Add 1 to this variable and print it
-        //Add 1 to this variable again and print it again
 
-        
-        
-        
+        byte myC = 127;
+        //Add 1 to this variable and print it
+        System.out.println(myC + 1);
+        //Add 1 to this variable again and print it again
+        System.out.println(myC + 1);
+
+
     }    
 }
