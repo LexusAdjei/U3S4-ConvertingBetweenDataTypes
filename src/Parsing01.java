f71ee83af06d0fdaa206783ad365637a5ecc9054
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2025-01-16 15:57:48 -0500

    Parsiing stuff..
    yep yap yap

--- src/Parsing01.java
+++ src/Parsing01.java
@@ -2,11 +2,16 @@ public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
         
-        
-        
-        
+        string shirtPrice = "15";
+        string taxRate = "0.05";
+        string gibberish = "887ds7nds87dsfs";
+
         //Parse shirtPrice and taxRate, and print the total tax
-        
+        int price = Interger.parseInt(shirtPrice);
+        double tax = Double.parseDouble(taxRate);
+        parse
+
+
         
         
         //Try to parse taxRate as an int
