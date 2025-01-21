77de665c37e45e6d0c975d8a647aa7f8e78ffcaf
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2025-01-21 13:35:06 -0500

    update to parsing01
    somethinsomethinsomethin

--- src/Parsing01.java
+++ src/Parsing01.java
@@ -1,23 +1,23 @@
 public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
-        string shirtPrice = "15";
-        string taxRate = "0.05";
-        string gibberish = "887ds7nds87dsfs";
+        String shirtPrice = "15";
+        String taxRate = "1.05";
+        String gibberish = "887ds7nds87dsfs";
 
         //Parse shirtPrice and taxRate, and print the total tax
-        int price = Interger.parseInt(shirtPrice);
+        int price = Integer.parseInt(shirtPrice);
         double tax = Double.parseDouble(taxRate);
-        parse
+        System.out.println("Total price after tax: " + (price * tax));
 
-
-        
-        
         //Try to parse taxRate as an int
-        
+        int parse = Integer.parseInt(taxRate);
+        System.out.println(parse);
+
         //Try to parse gibberish as an int
-        
+        int gib = Integer.parseInt(gibberish);
+        System.out.println(gib);
+
     }
-    
+
 }
